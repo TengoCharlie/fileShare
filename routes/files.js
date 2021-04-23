09eeba746a1b5e59b16dec0730463f1f721e6f27
Harsh Tenguriya <thegreatharsh1@gmail.com>
2021-04-23 18:31:53 +0530

    app base url name update typo

--- routes/files.js
+++ routes/files.js
@@ -86,7 +86,7 @@ router.post("/send", async (req, res) => {
     text: `${emailFrom} shared file with you`,
     html: emailTemplate({
       emailFrom: emailFrom,
-      downloadLink: `${process.env.APP_Base_URL}/files/${file.uuid}`,
+      downloadLink: `${process.env.APP_BASE_URL}/files/${file.uuid}`,
       size: parseInt(file.size / 1000) + " KB",
       expires: "24 hours",
     }),
